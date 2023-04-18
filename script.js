f0af38a6f520bc5dd16ce213ca64cbee50b33ae4
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-17 21:27:37 -0500

    refering to a website(mentioned in readme) and making some changes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var startButton = document.getElementById("start-button");
 
+
+var questionElements = document.getElementById('question-container');
+var topScores = document.getElementById('btn-scores');
+var timeLeft = document.getElementById('timer');
+
 startButton.addEventListener("click", function(){
     startQuiz();
 
